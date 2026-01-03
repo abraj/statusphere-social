4b74643a225e3b8bc10aaa9a130dfd628abcd6a1
Abhishek Raj <abhi@raj.me>
2026-01-03 11:49:01 +0700

    Add logger

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello..');
+import { pino } from 'pino'
+
+const run = () => {
+  const logger = pino({ name: 'server start' })
+
+  logger.info(`Server started..`);
+  console.log('Hello..');
+}
+
+run()
