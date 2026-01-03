24b10ba4f58f4abd22fbd99c830f6218343849ec
Abhishek Raj <abhi@raj.me>
2026-01-03 12:10:37 +0700

    Add env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { pino } from 'pino'
 
+import { env } from '#/lib/env'
+
 const run = () => {
+  const { NODE_ENV, HOST, PORT } = env
   const logger = pino({ name: 'server start' })
 
-  logger.info(`Server started..`);
+  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`)
   console.log('Hello..');
 }
 
